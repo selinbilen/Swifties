bf725555719d631b18e5be9532864114e903b371
NazliGuler <nguler1@binghamton.edu>
2021-05-13 02:33:00 +0300

    Added EndGameView, restart game and show final score functions

---
+++ Quiz Game/Quiz Game/EndViewController.swift
@@ -0,0 +1,45 @@
+//
+//  EndViewController.swift
+//  Quiz Game
+//
+//  Created by Nazlı Güler on 12.05.2021.
+//
+
+import UIKit
+
+class EndViewController: UIViewController {
+    
+    
+
+    
+    @IBOutlet weak var RestartButton: UIButton!
+    
+    @IBOutlet weak var ScoreLabel: UILabel!
+    
+    
+    var ScoreData:String!
+    
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        ScoreLabel.text = ScoreData
+    }
+    
+
+    @IBAction func RestartGame(_ sender: Any) {
+        
+        self.dismiss(animated: false, completion: nil)
+        self.presentingViewController?.dismiss(animated: false, completion: nil)
+        
+        
+        
+    }
+    
+
+}
+
